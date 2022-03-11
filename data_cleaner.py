c332871a9ab58ac0f1861017fdc2bdbca2f69d78
AlanRigden <101348794+AlanRigden@users.noreply.github.com>
2022-03-11 13:12:48 +0000

    Update data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -4,6 +4,9 @@ df = pd.read_csv('AAMSS.csv')
 
 def fill_blanks(df):
     df["Other monetary comp"].fillna(0, inplace = True)
+    df["Additional context on job title"].fillna('N/A', inplace = True)
+    df["Additional context on income"].fillna('N/A', inplace = True)
+    df["Currency - other"].fillna('N/A', inplace = True)
 
     return df
 
@@ -11,13 +14,13 @@ def convert_dtype(df):
     for x in df.index:
         df.loc[x,'Annual salary'] = int(df.loc[x,'Annual salary'].replace(',',''))
     
-    
     df['Annual salary'] = pd.to_numeric(df['Annual salary'])
     
     return df
 
 df = fill_blanks(df)
 df = convert_dtype(df)
-print(df['Annual salary'])
+print(df['Additional context on job title'])
 print(df.info())
+
 data = df
